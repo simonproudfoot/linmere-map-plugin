82283452f1c043a928ebc8fcf16e1b2a7a2b523f
simonproudfoot <simonproudfoot1980@gmail.com>
2022-02-22 12:45:42 +0000

    all hide/show logic

--- src/frontend/App.vue
+++ src/frontend/App.vue
@@ -1,8 +1,25 @@
 <template>
 <div class="map" id="vue-frontend-app" :style="{'background-image': 'url(' + require('./assets/map-back.jpg') + ')'}">
+
+    <!-- MAP TITLE -->
+    <div class="map__title text-green">
+        <div style="width: 124px; height:118px" :class="showFooter ? 'fillPink' : 'fillGreen'">
+            <span v-html="iconLeaf"></span>
+        </div>
+        <fade-transition>
+            <span v-show="!showFooter">
+                <h1>THIS IS</h1>
+                <h1>YOUR</h1>
+                <h1>LINMERE</h1>
+            </span>
+        </fade-transition>
+
+    </div>
+    <!-- MAP TITLE END -->
+
     <!-- MAP ELEMENTS -->
     <template v-for="(cat) in categorys">
-        <div v-for="(location) in cat.locations" :key="location.title" class="locations__marker" :style="{top:location.y+'px', left:+location.x+'px'}" @click="popUp(location)">
+        <div v-show="!category_selected || category_selected == cat" v-for="(location) in cat.locations" :key="location.title" class="locations__marker" :style="{top:location.y+'px', left:+location.x+'px'}" @click="popUp(location)">
             <div class="locations__marker__icon" :style="{backgroundColor:cat.iconColor}" v-html="cat.icon"></div>
             <h4 class="locations__marker__title">{{location.title}}</h4>
         </div>
@@ -10,57 +27,59 @@
     <!-- MAP ELEMENTS END -->
 
     <!--INFO WINDOW -->
-    <div v-if="Object.keys(location_selected).length" class="infoWindow" :style="{left: location_selected.x+'px'}">
-        <button class="infoWindow__close" @click="popUp()">
-            <div v-html="iconClose"></div>
-        </button>
-        <h3 class="infoWindow__title">{{location_selected.longTitle}}</h3>
-        <slider v-if="location_selected.images.length" class="infoWindow__slider" ref="slider" :options="options">
-            <slideritem v-for="(image,i) in location_selected.images" :key="i">
-                <img :src="image">
-            </slideritem>
-            <div slot="loading">loading...</div>
-        </slider>
-        <div class="infoWindow__inner">
-            <span v-for="(tab, key, i) in tabs" :key="i" class="infoWindow__inner__item" @click="selectTab(key)">
-                <div class="infoWindow__inner__item__arrow" v-html="iconVArrow" :style="tab ? 'transform: rotate(180deg)':null"></div>
-                <h3 class="infoWindow__inner__item__title">
-                    {{key.replaceAll('_',' ')}}
-                </h3>
-                <div v-if="tab" class="infoWindow__inner__item__content">
-                    <p v-html="location_selected.description">
-
-                    </p>
-                </div>
-            </span>
+    <fade-transition>
+        <div v-if="Object.keys(location_selected).length" class="infoWindow" :style="{left: location_selected.x+'px'}">
+            <button class="infoWindow__close" @click="popUp()">
+                <div v-html="iconClose"></div>
+            </button>
+            <h3 class="infoWindow__title">{{location_selected.longTitle}}</h3>
+            <slider v-if="location_selected.images.length" class="infoWindow__slider" ref="slider" :options="options">
+                <slideritem v-for="(image,i) in location_selected.images" :key="i">
+                    <img :src="image">
+                </slideritem>
+                <div slot="loading">loading...</div>
+            </slider>
+            <div class="infoWindow__inner">
+                <span v-for="(tab, key, i) in tabs" :key="i" class="infoWindow__inner__item" @click="selectTab(key)">
+                    <div class="infoWindow__inner__item__arrow" v-html="iconVArrow" :style="tab ? 'transform: rotate(180deg)':null"></div>
+                    <h3 class="infoWindow__inner__item__title">
+                        {{key.replaceAll('_',' ')}}
+                    </h3>
+                    <collapse-transition>
+                        <div v-if="tab" class="infoWindow__inner__item__content">
+                            <p v-html="location_selected.description">
+                            </p>
+                        </div>
+                    </collapse-transition>
+                </span>
+            </div>
         </div>
-    </div>
+    </fade-transition>
     <!-- INFO WINDOW END -->
 
     <!-- FOOTER -->
-    <ul class="map__footer">
-        <div v-for="(cat, i) in categorys" :key="i" class="map__footer__item " :class="cat == category_selected ? 'activeIcon' : 'inacitveIcon' " @click="category_selected = cat">
-            <div v-html="cat.icon"></div>
-            <h3 :style="cat == category_selected ? 'opacity: 1' : 'opacity: 0'">{{cat.title.replace('_',' ')}}</h3>
+    <slide-y-down-transition>
+        <div v-show="showFooter" class="map__footer">
+            <div v-for="(cat, i) in categorys" :key="i" class="map__footer__item " :class="cat == category_selected ? 'activeIcon' : 'inacitveIcon' " @click="category_selected = cat">
+                <div v-html="cat.icon"></div>
+                <h3 :style="cat == category_selected ? 'opacity: 1' : 'opacity: 0'">{{cat.title.replace('_',' ')}}</h3>
+            </div>
         </div>
-    </ul>
-    <div class="filterButton">
-        <h3>FILTER</h3>
-        <button>
-            <svg width="67px" height="67px" viewBox="0 0 67 67" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                <g id="Desktop" stroke="none" stroke-width="1" fill="#fff" fill-rule="evenodd">
-                    <g id="Interactive-map-opened" transform="translate(-50.000000, -963.000000)" fill="#FABDB8">
-                        <rect id="Rectangle" x="50" y="963" width="67" height="67"></rect>
-                    </g>
-                </g>
-            </svg>
-        </button>
-    </div>
+    </slide-y-down-transition>
+    <slide-x-left-transition>
+        <div class="filterButton" @click="showFooter = !showFooter">
+
+            <h3 class="text-green" v-show="!showFooter">FILTER</h3>
+
+            <button v-html="iconVArrow" :class="showFooter ? 'bg-pink' :'bg-green'" style="padding: 18px" class="btn-sq inactiveIcon" :style="!showFooter ? 'transform: rotate(270deg)' : 'transform: rotate(90deg)'"></button>
+        </div>
+    </slide-x-left-transition>
     <!-- FOOTER END -->
 </div>
 </template>
 
 <script>
+import { FadeTransition, SlideXLeftTransition, SlideYDownTransition, CollapseTransition, SlideYUpTransition } from 'vue2-transitions'
 import { slider, slideritem } from 'vue-concise-slider'
 export default {
     name: 'App',
@@ -79,16 +98,26 @@ export default {
         }
     },
     watch: {
+        showFooter() {
+            this.location_selected = []
+            this.category_selected = ''
+        },
         location_selected() {
             this.closeAllTabs()
         },
     },
     components: {
         slider,
-        slideritem
+        slideritem,
+        SlideXLeftTransition,
+        FadeTransition,
+        SlideYDownTransition,
+        SlideYUpTransition,
+        CollapseTransition
     },
     data: function () {
         return {
+            showFooter: false,
             location_selected: [],
             category_selected: '',
             options: {
@@ -102,6 +131,7 @@ export default {
             tabSelected: null,
             categorys: [{
                     title: 'houses',
+                    iconColor: '#000',
                     icon: require('./assets/SVG/houses.svg'),
                     locations: [{
                         title: 'gfdg',
@@ -110,8 +140,8 @@ export default {
                         openingTimes: '',
                         contactDetails: '',
                         images: [],
-                        x: 0,
-                        y: 0,
+                        x: 700,
+                        y: 200,
                     }],
                 },
                 {
@@ -167,13 +197,31 @@ export default {
                 }
             ],
             iconClose: require('./assets/SVG/close.svg'),
-            iconVArrow: require('./assets/SVG/v-arrow.svg')
+            iconVArrow: require('./assets/SVG/v-arrow.svg'),
+            iconLeaf: require('./assets/SVG/leaf.svg')
         }
     },
 }
 </script>
 
 <style>
+.bg-green {
+    background-color: #00745F;
+}
+
+.text-green {
+    color: #00745F;
+}
+
+.bg-pink {
+    background-color: #FABDB8;
+}
+
+.btn-sq {
+    width: 66px;
+    height: 66px;
+}
+
 .map {
     height: 1080px;
     width: 1920px;
@@ -195,7 +243,7 @@ export default {
     list-style: none;
     margin: 0 !important;
     display: flex;
-    padding-left: 249px;
+    padding-left: 200px;
 }
 
 .map__footer__item {
@@ -224,6 +272,16 @@ export default {
     margin: auto;
 }
 
+.fillPink {
+    fill: #FABDB8 !important;
+    color: #FABDB8 !important;
+}
+
+.fillGreen {
+    fill: #00745F !important;
+    color: #00745F !important;
+}
+
 .inactiveIcon * {
     fill: #fff !important;
     color: #fff !important;
@@ -234,19 +292,30 @@ export default {
     color: #FABDB8 !important;
 }
 
+button {
+    cursor: pointer;
+}
+
 .filterButton {
-    height: 168px;
-    width: 249px;
-    display: flex;
+    width: auto;
     position: absolute;
-    bottom: 0;
-    left: 0;
+    bottom: 50px;
+    left: 50px;
     font-size: 30px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+}
+
+.filterButton h3 {
+    margin: 0;
+    padding-bottom: 0;
+    padding-right: 30px;
+
 }
 
 .locations {
     position: relative;
-    border: 2px red dashed;
     height: 100%;
     width: 100%;
 }
@@ -320,6 +389,7 @@ export default {
 
 .infoWindow__inner__item__content {
     padding-top: 17px;
+    display: block;
 }
 
 .infoWindow__inner__item h3,
@@ -354,5 +424,15 @@ export default {
 .infoWindow__inner {
     padding: 0 20px 10px 20px;
     color: #005A44;
+}
+
+.map__title {
+    position: absolute;
+    top: 50px;
+    left: 50px;
+}
+
+.map__title h1 {
+    margin-bottom: 0;
 }
 </style>
